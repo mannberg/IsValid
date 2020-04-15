05af811ca9dd68d6bba27746527aad3d6bb14b6e
Anders Mannberg <anders.mannberg@softhouse.se>
2020-04-15 21:39:19 +0200

    Made struct public

--- Sources/IsValid/IsValid.swift
+++ Sources/IsValid/IsValid.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-struct IsValid {
-    static func email(_ string: String) -> Bool {
+public struct IsValid {
+    public static func email(_ string: String) -> Bool {
         string.matches(
             "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         )
     }
     
-    static func password(_ string: String) -> Bool {
+    public static func password(_ string: String) -> Bool {
         string.matches(
             "^(?=.*[A-Za-z])(?=.*[0-9])(?!.*[^a-zA-Z0-9_!@#$&*]).{8,20}$"
         )
